9aa40a9ec49443184b777e9c3a1e70793cbed79a
Shami1211 <it21203558@my.sliit.lk>
2024-04-07 23:05:29 +0530

    done

--- backend/app.js
+++ backend/app.js
@@ -6,11 +6,13 @@ const app = express();
 
 // Routes
 const PickupRouter = require("./Routes/PickupRoute"); // Corrected path to PickupRoute
+const DeliveryRouter = require("./Routes/DeliveryRoute");
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 app.use("/pickups", PickupRouter); // Updated route to use PickupRouter
+app.use("/deliveries", DeliveryRouter);
 
 // DB connection
 connectDB(app); // Pass the app instance to connectDB
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+//Pickups-User
 import AddPickup from "./Components/Delivery/User/AddDelivery/AddPickup";
 import UserPickups from "./Components/Delivery/User/Delivery/Pickups";
 import UserUpdatePickup from "./Components/Delivery/User/UpdatePickup/UpdatePickup";
-
+//Pickups-Admin
 import UpdatePickup from "./Components/Delivery/Admin/UpdatePickup/UpdatePickup";
 import Pickups from "./Components/Delivery/Admin/Delivery/Pickups";
 
-import Home from "./Components/Delivery/Home";
-
+//Home
+import PickupHome from "./Components/Delivery/Pickup-Home";
+import DeliveryHome from "./Components/Delivery/Delivery-Home";
 
 function App() {
   return (
     <div>
       <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/pickup" element={<PickupHome />} />
+      <Route path="/" element={<DeliveryHome />} />
+
         <Route path="/add" element={<AddPickup />} />
         <Route path="/pickups" element={<Pickups />} />
         <Route path="/pickups/update/:id" element={<UpdatePickup />} />
@@ -22,6 +26,10 @@ function App() {
 
         <Route path="/user_pickups" element={<UserPickups />} />
         <Route path="/user_pickups/update/:id" element={<UserUpdatePickup />} />
+
+
+
+        
       </Routes>
     </div>
   );
